663c82a6f5a63cc1db45aaf12d224923af018613
vitluchko <98816838+vitluchko@users.noreply.github.com>
2022-11-20 20:23:00 +0200

    Add files via upload

---
+++ subProject/Users.h
@@ -0,0 +1,17 @@
+//
+// Created by appetitus on 27.07.22.
+//
+
+#ifndef LINUXPREVIEW_USERS_H
+#define LINUXPREVIEW_USERS_H
+
+#include "IFileSystem.h"
+
+class Users {
+public:
+    void FileReading(IFileSystem& method);
+    void Countig(IFileSystem& method);
+};
+
+
+#endif //LINUXPREVIEW_USERS_H
